b34a95adb84d19b2767091b98b6e01998775c1b6
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-12-03 11:44:26 -0300

    Add files via upload

---
+++ assets/preload/custom_events/diehud.lua
@@ -0,0 +1,5 @@
+function onEvent(name)
+	if name == 'diehud' then
+    doTweenAlpha('bye', 'camHUD', 0, 0.5, 'linear');
+end
+end
---
+++ assets/preload/custom_events/hihud.lua
@@ -0,0 +1,5 @@
+function onEvent(name)
+	if name == 'hihud' then
+    doTweenAlpha('hi', 'camHUD', 1, 0.5, 'linear');
+end
+end
